7075da04aeacb4d9f9ccfb1f0de07803ed3789e5
Bohdan Shevchenko <bohdan.shevchenko2@nure.ua>
2021-07-07 18:39:08 +0300

    Add info

--- common.php
+++ common.php
@@ -2,6 +2,11 @@
 
 $people = [
   [ "name" => "Bogdan Onischenko", "Role": "Speaker" ],
+  [ "name" => "Alexey Goncharov", "Role": "Student" ],
+  [ "name" => "Vladyslav Cherniuk", "Role": "Student" ],
+  [ "name" => "Oleg Savenok", "Role": "Student" ],
+  [ "name" => "Yehor Hrytsenko",   "Role": "Student"],
   [ "name" => "Bohdan Shevchenko", "Role": "Student" ],	
+  [ "conflicted change" ],
 ];
 
