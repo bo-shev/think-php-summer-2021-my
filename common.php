c91e3539442ef492e2be1e12adc687f81dbcbf97
Artem Kuptsov <artem.kuptsov@nure.ua>
2021-07-07 18:31:02 +0300

    Add lastname at array

--- common.php
+++ common.php
@@ -2,16 +2,10 @@
 
 $people = [
   [ "name" => "Bogdan Onischenko", "Role": "Speaker" ],
-<<<<<<< HEAD
-<<<<<<< HEAD
   [ "name" => "Artem Kuptsov", "Role": "Student" ],
-=======
-=======
   [ "name" => "Vladyslav Cherniuk", "Role": "Student" ],
   [ "name" => "Oleg Savenok", "Role": "Student" ],
->>>>>>> 736397a46f9fd270440968e875e978fb7dfab486
   [ "name" => "Yehor Hrytsenko",   "Role": "Student"],
   [ "conflicted change" ],
->>>>>>> 664074510f5a174005dd3ddc03093162f80275bc
 ];
 
